be4fdf9e04f364f37901fcf4a059c33f60fe0baf
amindazad <amindazad@icloud.com>
2020-07-14 15:23:52 -0700

    added add_to_head method

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,9 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def delete(self):
+        pass
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +30,23 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        # create instance of ListNode with value
+        new_node  = ListNode(value, None, None)
+        # increment the DLL length attribute
+        self.length += 1
+        # if DLL is not empty
+            # set new node's next to current head
+            # set head's prev to new node
+            # set head to the new node
+        if self.head :
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        # if DLL is empty
+            # set head and tail to the new node instance
+        else:
+            self.head = new_node
+            self.tail = new_node
         
     """
     Removes the List's current head node, making the
@@ -36,6 +55,17 @@ class DoublyLinkedList:
     """
     def remove_from_head(self):
         pass
+        # store the value of the head
+        # decrement the length of the DLL
+        # delete the head
+            # if head.next is not None
+                # set head.next's prev to None
+                # set head to head.next
+            # else (if head.next is None)
+                # set head to None
+                # set tail to None
+
+        # return the value
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -44,6 +74,16 @@ class DoublyLinkedList:
     """
     def add_to_tail(self, value):
         pass
+        # create instance of ListNode with value
+        # increment the DLL length attribute
+
+        # if DLL is empty
+            # set head and tail to the new node instance
+        
+        # if DLL is not empty
+            # set new node's prev to current tail
+            # set tail's next to new node
+            # set tail to the new node
             
     """
     Removes the List's current tail node, making the 
@@ -52,6 +92,17 @@ class DoublyLinkedList:
     """
     def remove_from_tail(self):
         pass
+        # store the value of the tail
+        # decrement the length of the DLL
+        # delete the tail
+            # if tail.prev is not None
+                # set tail.prev's next to None
+                # set tail to tail.prev
+            # else (if tail.prev is None)
+                # set head to None
+                # set tail to None
+
+        # return the value
             
     """
     Removes the input node from its current spot in the 
--- stack/stack.py
+++ stack/stack.py
@@ -22,12 +22,12 @@ class Stack:
     def __len__(self):
         return self.size
 
-    def enqueue(self, value):
+    def push(self, value):
         # self.storage.append(value)
         self.storage.add_to_tail(value)
         self.size += 1
 
-    def dequeue(self):
+    def pop(self):
         # if self.size == 0 :
         #     return None
         # else:
